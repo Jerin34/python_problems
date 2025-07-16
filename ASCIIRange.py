a0be47b1a61a528f180385e000d9a65e0e0bcda2
Jerin Benny <jerinbenny875@gmail.com>
2025-07-16 21:35:39 +0530

    add program to print characters in given ASCII range

---
+++ ASCIIRange.py
@@ -0,0 +1,11 @@
+word = input()
+frist_num = int(input())
+last_num = int(input())
+s = ''
+for char in word:
+    s = ord(char)
+    # print(s)
+    for i in range(frist_num,last_num+1):
+        if i == s:
+            print(chr(i),end=' ')
+ 
